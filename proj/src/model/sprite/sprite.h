9515ed40de5f128b422fdea2f2239c3d9a09fa54
Leandro Resende <up202306343@up.pt>
2025-05-01 23:01:42 +0100

    Added sprite structure and corresponding functions

---
+++ proj/src/model/sprite/sprite.h
@@ -0,0 +1,64 @@
+#ifndef _SPRITE_H_
+#define _SPRITE_H_
+
+#include <lcom/lcf.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include "graphics.h"
+
+
+/** @defgroup sprite Sprite
+ * @{
+ *
+ * Sprite related functions
+ */
+
+struct sprite_imp;
+typedef struct sprite_imp Sprite;
+
+/**
+ * @brief Returns the width of the sprite
+ * 
+ * @param sp Pointer to the sprite
+ * @return The width of the sprite
+ */
+uint16_t get_sprite_width(Sprite *sp);
+
+/**
+ * @brief Returns the height of the sprite
+ * 
+ * @param sp Pointer to the sprite
+ * @return The height of the sprite
+ */
+uint16_t get_sprite_height(Sprite *sp);
+
+/** 
+ * @brief Creates a new sprite from XPM "pic"
+ * This function allocates memory for the sprite and loads the XPM image.
+ * It does not draw the sprite on the screen.
+ * 
+ * @param pic The XPM image to be loaded
+ * @return Pointer to the created sprite, or NULL if the pixmap is invalid
+*/
+Sprite *create_sprite(const xpm_map_t pic);
+
+/**
+ * @brief Frees the memory allocated for the sprite
+ * 
+ * @param sp Pointer to the sprite to be destroyed
+ */
+void destroy_sprite(Sprite *sp);
+
+/**
+ * @brief Draws the sprite on the screen at the specified coordinates
+ * 
+ * @param sp Pointer to the sprite to be drawn
+ * @param x The x-coordinate where the sprite will be drawn
+ * @param y The y-coordinate where the sprite will be drawn
+ */
+void draw_sprite(Sprite *sp, uint16_t x, uint16_t y);
+
+/** @} end of sprite */
+
+#endif
+
