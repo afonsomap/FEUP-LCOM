3dc844b4a7d3645b8331c2fc88b4868cca443780
Leandro Resende <up202306343@up.pt>
2025-05-01 23:02:03 +0100

    Added animated sprite structure and corresponding functions

---
+++ proj/src/model/animSprite/animatedSprite.c
@@ -0,0 +1,97 @@
+#include "animatedSprite.h"
+
+struct animatedSprite_imp {
+  uint16_t width, height; // dimensions
+  uint8_t **map;
+  uint8_t aspeed; // number of frames per animation
+  uint8_t num_images; // number of images in the animation
+};
+
+uint16_t get_anim_sprite_width(AnimSprite *sp) {
+  if (sp == NULL) {
+    return 0;
+  }
+  return sp->width;
+}
+
+uint16_t get_anim_sprite_height(AnimSprite *sp) {
+  if (sp == NULL) {
+    return 0;
+  }
+  return sp->height;
+}
+
+uint8_t get_anim_sprite_aspeed(AnimSprite *sp) {
+  if (sp == NULL) {
+    return 0;
+  }
+  return sp->aspeed;
+}
+
+uint8_t get_anim_sprite_num_images(AnimSprite *sp) {
+  if (sp == NULL) {
+    return 0;
+  }
+  return sp->num_images;
+}
+
+AnimSprite *create_anim_sprite(uint8_t no_pic, const xpm_map_t pic1, ...) {
+  // allocate space for the "object"
+  AnimSprite *asp = malloc(sizeof(AnimSprite));
+
+  // allocate array of pointers to pixmaps
+  asp->map = malloc(no_pic * sizeof(char *));
+
+  xpm_image_t img;
+  asp->map[0] = xpm_load(pic1, XPM_INDEXED, &img);
+  if (asp->map[0] == NULL) {
+      free(asp->map);
+      free(asp);
+      return NULL;
+  }
+  asp->width = img.width;
+  asp->height = img.height;
+  asp->num_images = no_pic;
+  asp->aspeed = 5; // default animation speed
+
+  // load pixmaps
+  va_list ap;
+  va_start(ap, pic1);
+  for (int i = 1; i < no_pic; i++) {
+      const char* tmp = va_arg(ap, const char *);
+      asp->map[i] = xpm_load((xpm_map_t)tmp, XPM_INDEXED, &img);
+      if (asp->map[i] == NULL || img.width != asp->width || img.height != asp->height) {
+          for (int j = 0; j < i; j++) {
+            free(asp->map[j]);
+          }
+          free(asp->map);
+          free(asp);
+          va_end(ap);
+          return NULL;
+      }
+  }
+  va_end(ap);
+  return asp;
+}
+
+
+void destroy_anim_sprite(AnimSprite *sp) {
+  if( sp == NULL ){
+    return;
+  }
+  for(int i = 0; i < sp->num_images; i++){
+    if( sp->map[i] ){
+      free(sp->map[i]);
+    }
+  }
+  free(sp->map);
+  free(sp);
+  sp = NULL; 
+}
+
+void draw_anim_sprite(AnimSprite *sp, uint16_t x, uint16_t y, uint8_t current_image) {
+  if (sp == NULL) {
+    return;
+  }
+  vg_draw_xpm(sp->map[current_image],x,y ,sp->width, sp->height);
+}
---
+++ proj/src/model/animSprite/animatedSprite.h
@@ -0,0 +1,84 @@
+#ifndef __ANIMATED_SPRITE_H
+#define __ANIMATED_SPRITE_H
+
+/**
+ * @defgroup animatedSprite Animated Sprite
+ * @{
+ * 
+ * Animated sprite related functions
+ */
+
+#include <lcom/lcf.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "sprite.h"
+
+struct animatedSprite_imp;
+typedef struct animatedSprite_imp AnimSprite;
+
+/**
+ * @brief Returns the width of the animated sprite
+ * 
+ * @param sp Pointer to the animated sprite
+ * @return The width of the animated sprite
+ */
+uint16_t get_anim_sprite_width(AnimSprite *sp);
+
+/**
+ * @brief Returns the height of the animated sprite
+ * 
+ * @param sp Pointer to the animated sprite
+ * @return The height of the animated sprite
+ */
+uint16_t get_anim_sprite_height(AnimSprite *sp);
+
+/**
+ * @brief Returns the animation speed of the animated sprite
+ * 
+ * @param sp Pointer to the animated sprite
+ * @return The animation speed of the animated sprite
+ */
+uint8_t get_anim_sprite_aspeed(AnimSprite *sp);
+
+/**
+ * @brief Returns the number of images in the animated sprite
+ * 
+ * @param sp Pointer to the animated sprite
+ * @return The number of images in the animated sprite
+ */
+uint8_t get_anim_sprite_num_images(AnimSprite *sp);
+
+
+/**
+ * @brief Creates a new animated sprite from XPM "pic"
+ * This function allocates memory for the animated sprite and loads the XPM images.
+ * It does not draw the animated sprite on the screen.
+ * 
+ * @param no_pic The number of images in the animation
+ * @param pic1 The first XPM image to be loaded
+ * @param ... The rest of the XPM images to be loaded
+ * @return Pointer to the created animated sprite, or NULL if the pixmap is invalid
+ */
+AnimSprite *create_anim_sprite(uint8_t no_pic, xpm_map_t pic1, ...);
+
+/**
+ * @brief Frees the memory allocated for the animated sprite
+ * 
+ * @param sp Pointer to the animated sprite to be destroyed
+ */
+void destroy_anim_sprite(AnimSprite *sp);
+
+/**
+ * @brief Draws the animated sprite on the screen at the specified coordinates
+ * 
+ * @param sp Pointer to the animated sprite to be drawn
+ * @param x The x-coordinate where the animated sprite will be drawn
+ * @param y The y-coordinate where the animated sprite will be drawn
+ * @param current_image The current frame of the animation
+ */
+void draw_anim_sprite(AnimSprite *sp, uint16_t x, uint16_t y, uint8_t current_image);
+
+/** @} end of sprite */
+
+#endif /* __ANIMATED_SPRITE_H */
