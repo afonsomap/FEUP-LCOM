8669c242ccaee9c96d4b05f09dc46c8f233877eb
Leandro Resende <up202306343@up.pt>
2025-05-19 23:59:22 +0100

    Implemented page flipping and made small improvements in performance

--- proj/src/controllers/graphics/graphics.c
+++ proj/src/controllers/graphics/graphics.c
@@ -3,7 +3,11 @@
 
 static vbe_mode_info_t mode_info;
 static uint8_t *video_mem;
-static uint8_t *buffer;
+static uint32_t screen_size;
+static uint8_t bytes_per_pixel;
+static uint8_t *front_buffer;
+static uint8_t *back_buffer;
+static uint8_t cur_buffer = 1; // 1 = front, 0 = back
 
 vbe_mode_info_t get_mode_info() {
   return mode_info;
@@ -23,62 +27,80 @@ void set_video_mode(uint16_t mode) {
 
 void map_graphics_vram(uint16_t mode) {
   vbe_get_mode_info(mode, &mode_info); 
+  
+  bytes_per_pixel = (mode_info.BitsPerPixel + 7) / 8;
+  screen_size = mode_info.BytesPerScanLine * mode_info.YResolution;
 
   struct minix_mem_range mr;
   memset(&mr, 0, sizeof(mr));
   mr.mr_base = (phys_bytes)mode_info.PhysBasePtr;
-  mr.mr_limit = mr.mr_base + mode_info.BytesPerScanLine * mode_info.YResolution;
+  mr.mr_limit = mr.mr_base + 2 * (screen_size);
 
   // Request permission for our process to map memory
   sys_privctl(SELF, SYS_PRIV_ADD_MEM, &mr);
 
   // Map memory
   video_mem = vm_map_phys(SELF, (void *)mr.mr_base, mr.mr_limit - mr.mr_base);
+  if (video_mem == MAP_FAILED) panic("Failed to map video memory");
+
+  front_buffer = video_mem;
+  back_buffer = video_mem + screen_size;
+  cur_buffer = 1;
 }
 
-void allocate_buffer(){
-  buffer = (uint8_t *)malloc(mode_info.BytesPerScanLine * mode_info.YResolution); 
-  if (buffer == NULL) {
-    panic("Failed to allocate buffer");
+void set_display_start(uint32_t addr) {
+
+  struct reg86 r;
+  memset(&r, 0, sizeof(r));
+  r.ax = 0x4F07;      
+  r.bx = 0x0000;      
+  r.cx = 0;      
+  r.dx = addr;      
+  r.intno = 0x10;  
+
+  if (sys_int86(&r) != OK) {
+    panic("set_display_start failed");
   }
 }
 
-void copy_buffer_vram(){
-  memcpy(video_mem, buffer, mode_info.BytesPerScanLine * mode_info.YResolution); 
+void vg_flip_buffer() {
+  if (cur_buffer) {
+    front_buffer = video_mem + screen_size;
+    back_buffer = video_mem;
+    cur_buffer = 0;
+    set_display_start(mode_info.YResolution);
+  } else {
+    front_buffer = video_mem;
+    back_buffer = video_mem + screen_size;
+    cur_buffer = 1;
+    set_display_start(0); 
+  }
+  
 }
 
-void free_buffer(){
-  if (buffer != NULL) {
-    free(buffer);
-    buffer = NULL;
-  }  
-}
-
-void clear_buffer(){
-  memset(buffer, 0, mode_info.BytesPerScanLine * mode_info.YResolution); 
+void clear_back_buffer() {
+  memset(back_buffer, 0, screen_size);
 }
 
 void draw_pixel(uint16_t x, uint16_t y, uint32_t color) {
   if (x >= mode_info.XResolution || y >= mode_info.YResolution) {
     panic("Coordinates out of bounds");
   }
-
-  unsigned bytesPixel = (mode_info.BitsPerPixel + 7) / 8;
   
-  unsigned int index = (mode_info.XResolution * y + x) * bytesPixel;
+  unsigned int index = (mode_info.XResolution * y + x) * bytes_per_pixel;
 
-  if (color == CHROMA_KEY_GREEN_888) { // Preciso definir uma cor para o transparente
+  if (color == CHROMA_KEY_GREEN_888) { 
     return; // Skip drawing transparent color
   }
 
-  if (memcpy(buffer + index, &color, bytesPixel) == NULL) {
+  if (memcpy(back_buffer + index, &color, bytes_per_pixel) == NULL) {
     panic("Failed to copy color to video memory");
   }
 }
 
 void draw_hline(uint16_t x, uint16_t y, uint16_t length, uint32_t color) {
   for (uint16_t i = 0; i < length; i++) {
-     draw_pixel(x + i, y, color);
+    draw_pixel(x + i, y, color);
   }
 }
 
@@ -93,17 +115,15 @@ int vg_draw_xpm(uint8_t* xpm_map, uint16_t x, uint16_t y, uint16_t width, uint16
     return 1;
   }
 
-  unsigned bytes_per_pixel = (mode_info.BitsPerPixel + 7) / 8;
+  uint8_t *pixel_ptr = xpm_map;
 
   for (uint16_t i = 0; i < height; i++) {
     for (uint16_t j = 0; j < width; j++) {
 
-      unsigned int index = (i * width + j) * bytes_per_pixel;
-
       uint32_t color = 0;
-      memcpy(&color, &xpm_map[index], bytes_per_pixel);
-
+      memcpy(&color, pixel_ptr, bytes_per_pixel);
       draw_pixel(x + j, y + i, color);
+      pixel_ptr += bytes_per_pixel;
     }
   }
 
