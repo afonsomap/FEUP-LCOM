80621ee262339df679c81700446923ec6146d66e
Afonso <up202305652@up.pt>
2025-05-29 12:05:53 +0100

    Fixed some things

--- proj/src/model/multiMode/multiMode.c
+++ proj/src/model/multiMode/multiMode.c
@@ -24,24 +24,24 @@ uint16_t EXIT_BTN_X = 20;
 uint16_t EXIT_BTN_Y = 20;
 
 MultiMode *create_multiMode(SpriteLoader *loader){
-  SingleMode *sm = (SingleMode *) malloc(sizeof(SingleMode));
-  if (sm == NULL) {
+  MultiMode *mm = (MultiMode *) malloc(sizeof(MultiMode));
+  if (mm == NULL) {
     return NULL;
   }
   
-  sm->x_initial_grid = 208; // Initialize grid position
-  sm->y_initial_grid = 52; // Initialize grid position
-  sm->grid_square_width = get_sprite_width(get_wall(loader)); // Get the width of the grid square
-  sm->grid_background = get_grid_background(loader); // Get the background sprite
-  sm->game_background = get_game_background(loader); // Get the game background sprite
-  sm->exit_button_sprite = get_exit(loader);
+  mm->x_initial_grid = 208; // Initialize grid position
+  mm->y_initial_grid = 52; // Initialize grid position
+  mm->grid_square_width = get_sprite_width(get_wall(loader)); // Get the width of the grid square
+  mm->grid_background = get_grid_background(loader); // Get the background sprite
+  mm->game_background = get_game_background(loader); // Get the game background sprite
+  mm->exit_button_sprite = get_exit(loader);
 
-  sm->player1 = create_player(1* sm->grid_square_width, 1 * sm->grid_square_width, get_player1_left(loader), get_player1_right(loader), get_player1_up(loader), get_player1_down(loader), get_player1_standing(loader));
+  mm->player1 = create_player(1* mm->grid_square_width, 1 * mm->grid_square_width, get_player1_left(loader), get_player1_right(loader), get_player1_up(loader), get_player1_down(loader), get_player1_standing(loader));
 
-  sm->player2 = create_player(15* sm->grid_square_width, 13 * sm->grid_square_width, get_player2_left(loader), get_player2_right(loader), get_player2_up(loader), get_player2_down(loader), get_player2_standing(loader));
+  mm->player2 = create_player(15* mm->grid_square_width, 13 * mm->grid_square_width, get_player2_left(loader), get_player2_right(loader), get_player2_up(loader), get_player2_down(loader), get_player2_standing(loader));
 
-  if (sm->player1 == NULL) {
-    free(sm);
+  if (mm->player1 == NULL || mm->player2 == NULL) {
+    free(mm);
     return NULL;
   }
 
@@ -51,26 +51,125 @@ MultiMode *create_multiMode(SpriteLoader *loader){
 
       if (i == 0 || i == GRID_HEIGHT - 1 || j == 0 || j == GRID_WIDTH - 1) {
         // Create indestructible walls at the borders
-        sm->wall_matrix[j][i] = create_wall(j, i, get_solid_wall(loader), 0, 1);
+        mm->wall_matrix[j][i] = create_wall(j, i, get_solid_wall(loader), 0, 1);
       } else if (i % 2 == 0 && j % 2 == 0) {
         // Create indestructible walls in the center
-        sm->wall_matrix[j][i] = create_wall(j, i, get_solid_wall(loader), 0, 1);
+        mm->wall_matrix[j][i] = create_wall(j, i, get_solid_wall(loader), 0, 1);
       }else if (i == 5 || j == 3) {
         // Create destructible walls in the center
-        sm->wall_matrix[j][i] = create_wall(j, i, get_wall(loader), 1, 1);
+        mm->wall_matrix[j][i] = create_wall(j, i, get_wall(loader), 1, 1);
       }else {
         // Create desctuctible walls in the center, but inactive
-        sm->wall_matrix[j][i] = create_wall(j, i, get_wall(loader), 1, 0);
+        mm->wall_matrix[j][i] = create_wall(j, i, get_wall(loader), 1, 0);
       }
 
 
-      sm->bomb_matrix[j][i] = create_bomb(j, i, get_bomb(loader), 0);
-      sm->explosion_matrix[j][i] = create_explosion(j, i, get_explosion(loader));
+      mm->bomb_matrix[j][i] = create_bomb(j, i, get_bomb(loader), 0);
+      mm->explosion_matrix[j][i] = create_explosion(j, i, get_explosion(loader));
     }
   }
 
-  sm->bomb_options = create_bomb_options(get_bomb_options(loader), get_selected_options(loader));
-  sm->player1_bomb_option = NORMAL;
+  mm->bomb_options = create_bomb_options(get_bomb_options(loader), get_selected_options(loader));
+  mm->player1_bomb_option = NORMAL;
+  mm->player2_bomb_option = NORMAL;
 
-  return sm;
+  return mm;
 }
+
+void destroy_multiMode(MultiMode *mm) {
+  if (mm == NULL) {
+    return;
+  }
+
+  // Destroy players
+  destroy_player(mm->player1);
+  destroy_player(mm->player2);
+
+  // Destroy walls, bombs, and explosions
+  for (int i = 0; i < GRID_HEIGHT; i++) {
+    for (int j = 0; j < GRID_WIDTH; j++) {
+      destroy_wall(mm->wall_matrix[j][i]);
+      destroy_bomb(mm->bomb_matrix[j][i]);
+      destroy_explosion(mm->explosion_matrix[j][i]);
+    }
+  }
+
+  // Destroy bomb options
+  destroy_bomb_options(mm->bomb_options);
+
+  free(mm);
+}
+
+void draw_multiMode(MultiMode *mm) {
+  if (mm == NULL) {
+    return;
+  }
+  
+  // Draw background
+  draw_sprite(mm->grid_background, mm->x_initial_grid + 33, mm->y_initial_grid + 50);
+
+  //Draw Exit Button
+  draw_sprite(mm->exit_button_sprite, EXIT_BTN_X, EXIT_BTN_Y);
+
+  // Draw walls, bombs, explosions and player
+  for (int i = 0; i < GRID_HEIGHT; i++) {
+    for (int j = 0; j < GRID_WIDTH; j++) {
+      draw_wall(mm->wall_matrix[j][i], mm->x_initial_grid, mm->y_initial_grid, mm->grid_square_width);
+      draw_bomb(mm->bomb_matrix[j][i], mm->x_initial_grid, mm->y_initial_grid, mm->grid_square_width);
+      draw_explosion(mm->explosion_matrix[j][i], mm->x_initial_grid, mm->y_initial_grid, mm->grid_square_width);
+    }
+
+    if (get_player_Ypos(mm->player1) / mm->grid_square_width == i) {
+      draw_player(mm->player1, mm->x_initial_grid, mm->y_initial_grid);
+    }
+
+    if (get_player_Ypos(mm->player2) / mm->grid_square_width == i) {
+      draw_player(mm->player2, mm->x_initial_grid, mm->y_initial_grid);
+    }
+  }
+
+  draw_bomb_options(mm->bomb_options, mm->player1_bomb_option);
+  draw_bomb_options(mm->bomb_options, mm->player2_bomb_option);
+}
+
+static bool check_wall_collision(MultiMode *mm, uint16_t x, uint16_t y) {
+  if (mm == NULL) {
+    return true;
+  }
+
+  //calculate the grids positions of the 4 corners of the player
+  uint16_t grid_x_top_left = x / mm->grid_square_width;
+  uint16_t grid_y_top_left = y / mm->grid_square_width;
+  
+  uint16_t grid_x_bottom_right = (x + (mm->grid_square_width-1)) / mm->grid_square_width;
+  uint16_t grid_y_bottom_right = (y + (mm->grid_square_width-1)) / mm->grid_square_width;
+
+  uint16_t grid_x_top_right = (x + mm->grid_square_width -1) / mm->grid_square_width;
+  uint16_t grid_y_top_right = y / mm->grid_square_width;
+
+  uint16_t grid_x_bottom_left = x / mm->grid_square_width;
+  uint16_t grid_y_bottom_left = (y + (mm->grid_square_width -1)) / mm->grid_square_width;
+  // Check if the player is out of bounds
+
+  if (grid_x_top_left < 0 || grid_x_bottom_right >= GRID_WIDTH || grid_y_top_left < 0 || grid_y_bottom_right >= GRID_HEIGHT) {
+    return true; // Out of bounds
+  }
+
+  if (mm->wall_matrix[grid_x_top_left][grid_y_top_left] == NULL || is_wall_active(mm->wall_matrix[grid_x_top_left][grid_y_top_left])) {
+    return true; // Wall at the position
+  }
+  if (mm->wall_matrix[grid_x_bottom_right][grid_y_bottom_right] == NULL || is_wall_active(mm->wall_matrix[grid_x_bottom_right][grid_y_bottom_right])) {
+    return true; // Wall at the position
+  }
+  if (mm->wall_matrix[grid_x_top_right][grid_y_top_right] == NULL || is_wall_active(mm->wall_matrix[grid_x_top_right][grid_y_top_right])) {
+    return true; // Wall at the position
+  }
+  if (mm->wall_matrix[grid_x_bottom_left][grid_y_bottom_left] == NULL || is_wall_active(mm->wall_matrix[grid_x_bottom_left][grid_y_bottom_left])) {
+    return true; // Wall at the position
+  }
+  return false; // No wall collision
+}
+
+
+// 0 Continue game
+// 1 Goes back to menu  
